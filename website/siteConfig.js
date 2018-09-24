afd808c1e76e0206b68170be42089c1cf011ae0a
Katie Martinez <martinek@newschool.edu>
2018-09-24 14:44:45 -0400

    add config files

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'The New School', // Title for your website.
   tagline: 'Archives & Special Collections Documentation',
-  url: 'https://katiemartinez.github.io', // Your website URL
-  baseUrl: '/docs/', // Base URL for your project */
+  url: 'https://tnsarchives.github.io', // Your website URL
+  baseUrl: '/documentation/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
-  organizationName: 'katiemartinez',
+  projectName: 'documentation',
+  organizationName: 'tnsarchives',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
